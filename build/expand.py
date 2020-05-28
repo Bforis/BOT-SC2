514397034fc69902de3e49e7d0778d0945073f1a
bforis <faradianboris@outlook.com>
2020-05-28 17:38:19 +0200

    try to correct bug expand

--- build/expand.py
+++ build/expand.py
@@ -11,18 +11,16 @@ from sc2.position import Point2
 
 async def Expand(self):
     ccs: Units = self.townhalls(UnitTypeId.COMMANDCENTER)
-    if not self.can_afford(UnitTypeId.COMMANDCENTER):
-        return
-    elif self.can_afford(UnitTypeId.COMMANDCENTER):
-        if self.units(UnitTypeId.COMMANDCENTER).amount < (
-                self.iteration / self.ITERATIONS_PER_MINUTE):
-            if (
-                    self.already_pending(UnitTypeId.COMMANDCENTER) == 0
-            ):  # get_next_expansion returns the position of the next possible expansion location where you can place a command center
-                location: Point2 = await self.get_next_expansion()
-                if location:
-                    # Now we "select" (or choose) the nearest worker to that found location
-                    worker: Unit = self.select_build_worker(location)
-                    if worker and self.can_afford(UnitTypeId.COMMANDCENTER):
-                        # The worker will be commanded to build the command center
-                        worker.build(UnitTypeId.COMMANDCENTER, location)
+    if self.units(UnitTypeId.COMMANDCENTER).amount < (
+            self.iteration / self.ITERATIONS_PER_MINUTE
+    ):
+        if (
+                self.already_pending(UnitTypeId.COMMANDCENTER) == 0
+        ):  # get_next_expansion returns the position of the next possible expansion location where you can place a command center
+            location: Point2 = await self.get_next_expansion()
+            if location:
+                # Now we "select" (or choose) the nearest worker to that found location
+                worker: Unit = self.select_build_worker(location)
+                if worker and self.can_afford(UnitTypeId.COMMANDCENTER):
+                    # The worker will be commanded to build the command center
+                    worker.build(UnitTypeId.COMMANDCENTER, location)
